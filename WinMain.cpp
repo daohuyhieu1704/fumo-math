e4eb16c772f3462e3c1348794baae543116f212c
HP\daohu <daohuyhieu1704@gmail.com>
2024-04-27 18:35:17 +0700

    init window

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,30 +1,104 @@
-#include <Windows.h>
+#include "pch.h"
 
-		#define MAX_NAME_LENGTH 256
-		
-		WCHAR			WindowClass[MAX_NAME_LENGTH];
+/*------------------------------------------------------------------------------*/
+/*	Global var																	*/
+/*------------------------------------------------------------------------------*/
+#pragma region Global var
+WCHAR			WindowClass[MAX_NAME_LENGTH];
+WCHAR			WindowTitle[MAX_NAME_LENGTH];
+HICON			hIcon;
+#pragma endregion
+/*------------------------------------------------------------------------------*/
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, int nCmdShow)
+
+
+LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	wcscpy_s(WindowClass, MAX_NAME_LENGTH, L"FumoEngineTest");
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
 
-	WNDCLASSEX wc;
+
+/*------------------------------------------------------------------------------*/
+/*	Declarations																*/
+/*------------------------------------------------------------------------------*/
+#pragma region Declarations
+VOID			InitialVariables();
+VOID			CreateWindowClass();
+VOID			InitWindow();
+VOID			MsgLoop();
+#pragma endregion
+/*------------------------------------------------------------------------------*/
+
+
+int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR lpCmdLine, int nCmdShow)
+{
+	InitialVariables();
+	CreateWindowClass();
+	InitWindow();
+	MsgLoop();
+
+	return 0;
+}
+
+VOID InitialVariables()
+{
+	LoadString(HInstance(), IDS_APPNAME, WindowClass, MAX_NAME_LENGTH);
+	LoadString(HInstance(), IDS_APPTITLE, WindowTitle, MAX_NAME_LENGTH);
+	hIcon = LoadIcon(HInstance(), MAKEINTRESOURCE(IDI_MAINICON));
+}
+
+VOID CreateWindowClass()
+{
+	WNDCLASSEX wc{};
 	wc.cbSize = sizeof(WNDCLASSEX);
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
-
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-
+	wc.hIcon = hIcon;
+	wc.hIconSm = hIcon;
 	wc.lpszClassName = WindowClass;
 
 	wc.lpszMenuName = nullptr;
 
-	wc.hInstance = hInstance;
+	wc.hInstance = HInstance();
 
-	return 0;
+	wc.lpfnWndProc = WinProc;
+
+	RegisterClassEx(&wc);
+}
+
+VOID InitWindow()
+{
+	HWND hWnd = CreateWindow(WindowClass, WindowTitle, WS_OVERLAPPEDWINDOW, 0, 0, 800, 600, nullptr, nullptr, HInstance(), nullptr);
+
+	if (!hWnd)
+	{
+		MessageBox(NULL, L"Window Creation Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
+		return VOID();
+	}
+
+	ShowWindow(hWnd, SW_SHOW);
+}
+
+VOID MsgLoop()
+{
+	MSG msg = { 0 };
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 }
